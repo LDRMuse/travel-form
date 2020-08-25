0e6289da23790d0e49d3cd30747afa454b602a34
Melissa <melissakheying@gmail.com>
2020-08-25 09:59:32 -0500

    Create Login component and use React Router to display

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React, { Fragment } from 'react';
 
 import {Header, Form} from "./components"
 
+import { Header, Form, Login } from "./components"
 import "./App.css";
 
 export const App = () => (
-         <Fragment>
-           <Header />
-           <Form />
-         </Fragment>
-       )
+  <Router>
+    <Route exact={true} path="/">
+      <Header />
+      <Form />
+    </Route>
+
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Switch>
+</Router>
+)
 
