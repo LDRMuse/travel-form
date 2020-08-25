fd4b01e8efdb6928ab617bb4711224579d183d26
Melissa <melissakheying@gmail.com>
2020-08-25 10:25:33 -0500

    create login form

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react'
 export const Header = () => (
          <header className="hero is-primary">
            <div className="hero-body">
-             <div className="container">
+             <div className="container has-text-centered">
                <h1 className="has-text-centered title">Register for Travel</h1>
+               <button className=" button is-warning mt-3">Login</button>
              </div>
            </div>
          </header>
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const Login = () => {
   return (
-    <p>login</p>
+    <form>
+      <label htmlFor="email"></label>
+        <input type="email" placeholder="Email"></input>
+        <button className="button is-warning mt-3">Get Travel Plans</button>
+</form>
   )
+}
+
+Login.propTypes = {
+  value: PropTypes.string.isRequired,
+  id: PropTypes.string,
 }
