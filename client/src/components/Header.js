dcdc5b1f6eabfe8ae6a671bdb684d5fc2d9dc346
Melissa <melissakheying@gmail.com>
2020-08-25 15:24:45 -0500

    Only show login button if on home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export const Header = () => {
+  const location = useLocation()
 
 
 
-return (
-  <header className="hero is-primary">
-    <div className="hero-body">
-      <div className="container has-text-centered">
-        <h1 className="has-text-centered title">Register for Travel</h1>
-        <Link to="/login" className="mr-2">
-        <button className=" button is-warning mt-3">Login</button>
-    </Link>
+  return (
+    <header className="hero is-primary">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+
+          <Link to="/">
+            <h1 className="has-text-centered title">Register for Travel</h1>
+          </Link>
+          {location.pathname === '/' ?
+            <Link to="/login" className="mr-2">
+              <button className=" button is-warning mt-3">Login</button>
+            </Link> :
+            null
+          }
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
 }
